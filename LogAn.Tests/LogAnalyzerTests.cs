0d19c3d5636c2791958d8c4150870ab631773ab6
Tu Tong <ttong@avatar-computing.com>
2018-05-10 22:07:21 -0400

    Setup the NUnit test project.

---
+++ LogAn.Tests/LogAnalyzerTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace LogAn.Tests
+{
+    [TestFixture]
+    public class LogAnalyzerTests
+    {
+        private LogAnalyzer m_analyzer = null;
+
+        [SetUp]
+        public void Setup()
+        {
+
+            m_analyzer = new LogAnalyzer();
+        }
+
+        [Test]
+        [Ignore("This test is broken")]
+        public void IsValidFileName_validFileLowerCased_ReturnsTrue()
+        {
+            bool result = m_analyzer.IsValidLogFileName("whatever.slf");
+
+            Assert.IsTrue(result, "filename should be valid!");
+        }
+
+    }
+}
